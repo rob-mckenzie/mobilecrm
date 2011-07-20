102f45ba614029355ade7fad1e820c82c7e732ff
Rob McKenzie <rob.mckenzie@sealedair.com>
2011-07-20 23:59:17 +0000

    ALT_CSS_DSP_015

--- src/js/com.sealedair.mobile.xTemplates.js
+++ src/js/com.sealedair.mobile.xTemplates.js
@@ -228,7 +228,8 @@ mobilens.xTplOrdersTest_old = ''+
 
 mobilens.xTplOrdersTest = ''+
     '<tpl for="."><div>'+
-' <span class="headerLabel">Order:&nbsp;&nbsp;&nbsp; </span><span class="headerDetail expandTest">{documentNumberTrim}</span><br>'+
+' <span class="headerLabel">Order:&nbsp;&nbsp;&nbsp; </span><span class="headerDetail expandTest">{documentNumberTrim}</span>'+
+' <span class="headerLabel">&nbsp;&nbsp;&nbsp;Downloaded Items:&nbsp;&nbsp;&nbsp; </span><span class="headerDetail expandTest">{itemCount}</span><br>'+
 ' <span class="headerLabel">Ship To:&nbsp;&nbsp;&nbsp;</span><span class="headerDetail">{shipToName} [{shipToNo}] [{salesOrg}][{division}], {shipToCity}</span><br>'+
 ' <span class="headerLabel">Sold To:&nbsp;&nbsp;&nbsp;</span><span class="headerDetail">{soldToName} [{soldTo}] [{salesOrg}][{division}], {soldToCity}</span><br>'+
 ' <span class="headerLabel">Doc Type:&nbsp;&nbsp;&nbsp;</span><span class="headerDetail">{salesDocumentType}&nbsp;&nbsp;&nbsp&nbsp;&nbsp;'+
