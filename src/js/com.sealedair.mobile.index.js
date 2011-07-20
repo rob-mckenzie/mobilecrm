bd5157081055ba73e41094f2e8e19ba86493dbb9
Rob McKenzie <rob.mckenzie@sealedair.com>
2011-07-20 21:38:03 +0000

    ALT_CSS_DSP_007

--- src/js/com.sealedair.mobile.index.js
+++ src/js/com.sealedair.mobile.index.js
@@ -715,8 +715,9 @@ SACCRM.Main = {
     	this.ui = new Ext.ux.UniversalUI();
         mobilens.storeSAPOrders.sortBy('') //note: passing in an emptry string to this function will sort the list by its default field...rmJr
     	mobilens.orderList.refreshDisplay('');
-        mobilens.orderList.scroller.momentum = false;
-        /*
+        
+//        mobilens.orderList.scroller.momentum = false;
+/*
         mobilens.orderList.scroller.acceleration = 30;
         mobilens.orderList.scroller.friction = 0;
         mobilens.orderList.scroller.fps = 30;        
